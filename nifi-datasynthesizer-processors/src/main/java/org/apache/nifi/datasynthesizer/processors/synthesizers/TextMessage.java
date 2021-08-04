f2a7746b53b767eade174e67013b4d81e7f7864a
phrocker <phrocker@apache.org>
2021-08-04 11:39:20 -0400

    Add Text Message Sampler

---
+++ nifi-datasynthesizer-processors/src/main/java/org/apache/nifi/datasynthesizer/processors/synthesizers/TextMessage.java
@@ -0,0 +1,76 @@
+package org.apache.nifi.datasynthesizer.processors.synthesizers;
+
+import org.apache.nifi.annotation.lifecycle.OnScheduled;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.components.ValidationContext;
+import org.apache.nifi.components.ValidationResult;
+import org.apache.nifi.datasynthesizer.processors.DataSynthesizer;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.util.StandardValidators;
+import org.apache.nifi.serialization.RecordSetWriterFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class PhoneNumber extends DataSynthesizer {
+    protected static final PropertyDescriptor AREA_CODE = new PropertyDescriptor.Builder()
+            .name("area-code")
+            .displayName("Area Code")
+            .description("Specifies an area code")
+            .addValidator(StandardValidators.INTEGER_VALIDATOR)
+            .required(false)
+            .build();
+
+
+
+    public PhoneNumber(){
+
+    }
+
+
+    @OnScheduled
+    @Override
+    public void onScheduled(final ProcessContext context) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        sampler = new ThreadLocal<>();
+        int area_min = 100;
+        int area_max = 999;
+        if (context.getProperty(AREA_CODE).isSet()){
+            final Integer value = context.getProperty(AREA_CODE).asInteger();
+            area_min = value;
+            area_max = value;
+        }
+        definedSchema = "[{\"name\": \"phone_number\", \"class\": \"join\", \"separator\": \"-\", \"value\": {\n" +
+                "          \"class\":\"sequence\",\n" +
+                "          \"length\":3,\n" +
+                "          \"array\":[\n" +
+                "              { \"class\": \"int\", \"min\": " +String.valueOf(area_min) + ", \"max\": " + String.valueOf(area_max) + "},\n" +
+                "              { \"class\": \"int\", \"min\": 100, \"max\": 999},\n" +
+                "              { \"class\": \"int\", \"min\": 1000, \"max\": 9999}\n" +
+                "          ]\n" +
+                "    }}]";
+
+
+    }
+
+    @Override
+    public List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        final List<PropertyDescriptor> properties = super.getSupportedPropertyDescriptors();
+        properties.add(AREA_CODE);
+        return properties;
+    }
+
+    @Override
+    protected Collection<ValidationResult> customValidate(ValidationContext validationContext) {
+        Collection<ValidationResult> set = Collections.emptySet();
+        if (validationContext.getProperty(AREA_CODE).isSet()){
+            final String strVal = validationContext.getProperty(AREA_CODE).getValue();
+            final Integer value = validationContext.getProperty(AREA_CODE).asInteger();
+            if (value < 0 || value > 999){
+                set.add(new ValidationResult.Builder().valid(false).explanation("Area code must be a 3 digit number").input(strVal).subject("Area code").build());
+            }
+        }
+        return set;
+    }
+}
---
+++ nifi-datasynthesizer-processors/src/test/java/org/apache/nifi/datasynthesizer/processors/synthesizers/TestTextMessage.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.datasynthesizer.processors.synthesizers;
+
+import com.github.javafaker.Faker;
+import org.apache.nifi.datasynthesizer.processors.DataSynthesizer;
+import org.apache.nifi.datasynthesizer.processors.synthesizers.PhoneNumber;
+import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.serialization.record.MockRecordWriter;
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class TestPhoneNumber {
+
+
+    private TestRunner getTestRunner(String area_code) throws InitializationException {
+        MockRecordWriter writerService = new MockRecordWriter("", false);
+        final TestRunner runner = TestRunners.newTestRunner(PhoneNumber.class);
+        runner.enforceReadStreamsClosed(false);
+        if (null != area_code) {
+            runner.setProperty(PhoneNumber.AREA_CODE, area_code);
+        }
+        runner.setProperty(DataSynthesizer.RECORD_COUNT,"1");
+        runner.addControllerService("writer", writerService);
+        runner.enableControllerService(writerService);
+        runner.setProperty(DataSynthesizer.RECORD_WRITER,"writer");
+        return runner;
+    }
+
+
+
+
+    @Test
+    public void testValidPhoneNumberWithAreaCode() throws Exception {
+        final String schema = "{'name':'br', 'class':'browser'}";
+        TestRunner runner = getTestRunner(schema);
+        runner = getTestRunner("497");
+        runner.assertValid();
+        runner.run();
+
+        runner.assertAllFlowFilesTransferred(DataSynthesizer.REL_SUCCESS, 1);
+        final MockFlowFile out = runner.getFlowFilesForRelationship(DataSynthesizer.REL_SUCCESS).get(0);
+        String phoneNumber = out.getContent().trim();
+        Pattern pattern = Pattern.compile("^497-(\\d{3}[- .]?)\\d{4}$");
+        Assert.assertTrue(out.getContent() + " Is not a valid phone number",pattern.matcher(phoneNumber).matches());
+    }
+
+    @Test
+    public void testValidPhoneNumberWithoutAreaCode() throws Exception {
+        final String schema = "{'name':'br', 'class':'browser'}";
+        TestRunner runner = getTestRunner(schema);
+        runner = getTestRunner(null);
+        runner.assertValid();
+        runner.run();
+
+        runner.assertAllFlowFilesTransferred(DataSynthesizer.REL_SUCCESS, 1);
+        final MockFlowFile out = runner.getFlowFilesForRelationship(DataSynthesizer.REL_SUCCESS).get(0);
+        String phoneNumber = out.getContent().trim();
+        Pattern pattern = Pattern.compile("^(\\d{3}[- .]?){2}\\d{4}$");
+        Assert.assertTrue(out.getContent() + " Is not a valid phone number",pattern.matcher(phoneNumber).matches());
+
+    }
+
+}
